3cfbc326ac2eb8f39cb46e93bfeb497f635b8749
elvece <lvcifferello@gmail.com>
2015-07-13 12:05:18 -0600

    added comment from mike's tip

--- main.js
+++ main.js
@@ -8,3 +8,5 @@ var j = i(2,3);
 var k = i(4,5);
 //prints to browser
 console.log(j,k);
+
+/* two functions, 1. return value 2. console log it 3. do a conditional where if (function1()===undefined console.log("function doesnt return value")else "function returns value")
