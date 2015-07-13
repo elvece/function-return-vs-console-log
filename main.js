409e3d3ef64cf2e1df8d14f07a9e627f7ab011cd
elvece <lvcifferello@gmail.com>
2015-07-13 11:08:55 -0600

    first attempt solution

---
+++ main.js
@@ -0,0 +1,9 @@
+var i = function(a,b){
+  var c = a + b;
+  //returns the value inside the function but does not print anything to the screen
+  return c;
+}
+
+var j = i(2,3);
+//prints to browers
+console.log(j);
